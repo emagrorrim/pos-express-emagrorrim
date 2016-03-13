3ff758d153fc93db50185473a6ca7e6d6bcf8759
emagrorrom <373407641@qq.com>
2016-03-13 14:41:47 +0800

    add cors dependency

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+var cors = require('cors')
 var express = require('express');
 var app = express();
+app.use(cors());
 
-app.get('/', function (req, res) {
-  res.sendfile('./package.json');
+app.get('/', function(req, res) {
+  res.sendfile('./front/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.get('/allItems', function (req, res) {
+  var allItems = [
+    { barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00 },
+    { barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00 },
+    { barcode: 'ITEM000002', name: '苹果', unit: '斤', price: 5.50 },
+    { barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15.00 },
+    { barcode: 'ITEM000004', name: '电池', unit: '个', price: 2.00 },
+    { barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.50 }
+  ];
+  res.send(JSON.stringify(allItems));
+  console.log(req + res);
+});
+
+app.listen(8080, function () {
+  console.log('App listening on port 8080!');
 });
--- front/js/storage/storage.js
+++ front/js/storage/storage.js
@@ -1,16 +1,26 @@
+var url = 'http://localhost:8080'
 function Storage() {}
 
 Storage.getAllItems = function(callBack) {
-  var allItems = [
-    { barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00 },
-    { barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00 },
-    { barcode: 'ITEM000002', name: '苹果', unit: '斤', price: 5.50 },
-    { barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15.00 },
-    { barcode: 'ITEM000004', name: '电池', unit: '个', price: 2.00 },
-    { barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.50 }
-  ];
-  Storage.setLocalAllItems(allItems);
-  callBack(allItems);
+  var allItems;
+  $.ajax({
+    method: "GET",
+    url: url + '/allItems',
+  })
+  .done(function( data ) {
+    allItems = JSON.parse(data);
+    Storage.setLocalAllItems(allItems);
+    callBack(allItems);
+  });
+  // var allItems = [
+  //   { barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00 },
+  //   { barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00 },
+  //   { barcode: 'ITEM000002', name: '苹果', unit: '斤', price: 5.50 },
+  //   { barcode: 'ITEM000003', name: '荔枝', unit: '斤', price: 15.00 },
+  //   { barcode: 'ITEM000004', name: '电池', unit: '个', price: 2.00 },
+  //   { barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.50 }
+  // ];
+  
 }
 
 Storage.setLocalAllItems = function(allItems) {
